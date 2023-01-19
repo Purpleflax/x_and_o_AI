0805d4bc9d25ddc35e537e587e8c77bb48659cb6
Jeffrey Reynolds <reynoldsjeffrey414@gmail.com>
2023-01-19 18:54:34 +0000

    Add start of minimax AI

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,4 @@
-const handleMenu = (() => {
+const handleMenu = () => {
   let difficulty;
   const startButtonP = document.getElementById("fader");
   startButtonP.addEventListener("click", () => {
@@ -17,13 +17,13 @@ const handleMenu = (() => {
     playerTwo.status = "AI";
     return { playerOne, playerTwo };
   });
-  const fadeInGame = (() => {
+  const fadeInGame = () => {
     const main = document.getElementById("main");
     main.style.visibility = "visible";
     main.style.opacity = "1";
     game.startGame();
-  });
-  const aiDifficulty = (() => {
+  };
+  const aiDifficulty = () => {
     const selector = document.getElementById("menuContainer");
     selector.style.visibility = "visible";
     selector.style.opacity = "1";
@@ -51,9 +51,9 @@ const handleMenu = (() => {
       difficulty = "hard";
       setTimeout(fadeInGame, 1000);
     });
-  });
+  };
   return { difficulty };
-});
+};
 
 const playerFactory = (name, status) => {
   const currentlyActive = false;
@@ -71,108 +71,116 @@ const displayController = (() => {
   const board = gameBoard.board;
   const renderBoard = () => {
     const playerGame = () => {
-    switchActiveImg();
-    playerTwoPic.innerHTML = `<img src="./images/person.svg" alt="X" class="playerImg">`;
-    board.forEach((item, index) => {
-      const clicked = document.getElementById(`cell-${index}`);
-      clicked.addEventListener("click", () => {
-        if ((playerOne.currentlyActive == true && board[index] == "")) {
-          clicked.textContent = "X";
-          board[index] = "X";
-          console.log(board);
-          playerOne.currentlyActive = false;
-          playerTwo.currentlyActive = true;
-          switchActiveImg();
-          document.getElementById("turnDisplay").innerText = "Player Two's Turn.";
-          game.checkWinner();
-        } else if (playerTwo.currentlyActive == true && board[index] == "") {
+      switchActiveImg();
+      playerTwoPic.innerHTML = `<img src="./images/person.svg" alt="X" class="playerImg">`;
+      board.forEach((item, index) => {
+        const clicked = document.getElementById(`cell-${index}`);
+        clicked.addEventListener("click", () => {
+          if (playerOne.currentlyActive == true && board[index] == "") {
+            clicked.textContent = "X";
+            board[index] = "X";
+            console.log(board);
+            playerOne.currentlyActive = false;
+            playerTwo.currentlyActive = true;
+            switchActiveImg();
+            document.getElementById("turnDisplay").innerText =
+              "Player Two's Turn.";
+            game.checkWinner();
+          } else if (playerTwo.currentlyActive == true && board[index] == "") {
             clicked.textContent = "O";
-            board[index] = "O"
+            board[index] = "O";
             console.log(board);
             playerOne.currentlyActive = true;
             playerTwo.currentlyActive = false;
             switchActiveImg();
-            document.getElementById("turnDisplay").innerText = "Player One's Turn.";
+            document.getElementById("turnDisplay").innerText =
+              "Player One's Turn.";
             game.checkWinner();
-        };
+          }
+        });
       });
-    });
+    };
+    const aiGame = () => {
+      playerTwoPic.innerHTML = `<img src="./images/ai.svg" alt="X" class="playerImg">`;
+      switchActiveImg();
+      board.forEach((item, index) => {
+        const clicked = document.getElementById(`cell-${index}`);
+        clicked.addEventListener("click", () => {
+          if (playerOne.currentlyActive == true && board[index] == "") {
+            clicked.textContent = "X";
+            board[index] = "X";
+            console.log(board);
+            playerOne.currentlyActive = false;
+            playerTwo.currentlyActive = true;
+            switchActiveImg();
+            document.getElementById("turnDisplay").innerText =
+              "Player Two's Turn.";
+            game.checkWinner();
+          }
+          if (difficulty === "easy") {
+            setTimeout(() => {
+              aiRandom = Math.floor(Math.random() * 9);
+              if (board[aiRandom] !== "" && board.includes("") == true) {
+                while (board[aiRandom] !== "") {
+                  aiRandom = Math.floor(Math.random() * 9);
+                }
+              }
+              if (playerTwo.currentlyActive == true && board[aiRandom] == "") {
+                const aiClick = document.getElementById(`cell-${aiRandom}`);
+                aiClick.textContent = "O";
+                board[aiRandom] = "O";
+                console.log(board);
+                playerOne.currentlyActive = true;
+                playerTwo.currentlyActive = false;
+                switchActiveImg();
+                document.getElementById("turnDisplay").innerText =
+                  "Player One's Turn.";
+                game.checkWinner();
+              }
+            }, 1000);
+          }
+          if (difficulty === "medium") {
+            setTimeout(() => {
+              aiRandom = Math.floor(Math.random() * 9);
+              if (board[aiRandom] !== "" && board.includes("") == true) {
+                while (board[aiRandom] !== "") {
+                  aiRandom = Math.floor(Math.random() * 9);
+                }
+              }
+              if (playerTwo.currentlyActive == true && board[aiRandom] == "") {
+                const aiClick = document.getElementById(`cell-${aiRandom}`);
+                aiClick.textContent = "O";
+                board[aiRandom] = "O";
+                console.log(board);
+                playerOne.currentlyActive = true;
+                playerTwo.currentlyActive = false;
+                switchActiveImg();
+                document.getElementById("turnDisplay").innerText =
+                  "Player One's Turn.";
+                game.checkWinner();
+              }
+            }, 1000);
+          }
+          if (difficulty === "hard") {
+            // minimax algorithm
+            let intelliCheck = true;
+            let bestScore = -Infinity;
+            const toMaxArray = Array.from(board);
+          }
+        });
+      });
+    };
+    return { playerGame, aiGame };
   };
-  const aiGame = () => {  
-    playerTwoPic.innerHTML = `<img src="./images/ai.svg" alt="X" class="playerImg">`;
-    switchActiveImg();
-    board.forEach((item, index) => {
-      const clicked = document.getElementById(`cell-${index}`);
-      clicked.addEventListener("click", () => {
-        if ((playerOne.currentlyActive == true && board[index] == "")) {
-          clicked.textContent = "X";
-          board[index] = "X";
-          console.log(board);
-          playerOne.currentlyActive = false;
-          playerTwo.currentlyActive = true;
-          switchActiveImg();
-          document.getElementById("turnDisplay").innerText = "Player Two's Turn.";
-          game.checkWinner();
-        };
-        if (difficulty === "easy") {
-        setTimeout(() => {
-        aiRandom = Math.floor(Math.random() * 9);
-        if (board[aiRandom] !== "" && board.includes("") == true) {
-          while (board[aiRandom] !== "") {
-            aiRandom = Math.floor(Math.random() * 9);
-          };
-        };
-        if (playerTwo.currentlyActive == true && board[aiRandom] == "") {
-          const aiClick = document.getElementById(`cell-${aiRandom}`);
-          aiClick.textContent = "O";
-          board[aiRandom] = "O"
-          console.log(board);
-          playerOne.currentlyActive = true;
-          playerTwo.currentlyActive = false;
-          switchActiveImg();
-          document.getElementById("turnDisplay").innerText = "Player One's Turn.";
-          game.checkWinner();
-        };
-      }, 1000);
-    }
-    if (difficulty === "medium") {
-      setTimeout(() => {
-        aiRandom = Math.floor(Math.random() * 9);
-        if (board[aiRandom] !== "" && board.includes("") == true) {
-          while (board[aiRandom] !== "") {
-            aiRandom = Math.floor(Math.random() * 9);
-          };
-        };
-        if (playerTwo.currentlyActive == true && board[aiRandom] == "") {
-          const aiClick = document.getElementById(`cell-${aiRandom}`);
-          aiClick.textContent = "O";
-          board[aiRandom] = "O"
-          console.log(board);
-          playerOne.currentlyActive = true;
-          playerTwo.currentlyActive = false;
-          switchActiveImg();
-          document.getElementById("turnDisplay").innerText = "Player One's Turn.";
-          game.checkWinner();
-        };
-      }, 1000);
-    }
-    if (difficulty === "hard") {
-      // minimax algorithm
-    }
-  });
-    });
-  };
-  return { playerGame, aiGame };
-};
-  const clearBoard = (() => {
+  const clearBoard = () => {
     switchActiveImg();
     board.forEach((item, index) => {
       const toClear = document.getElementById(`cell-${index}`);
       toClear.innerText = "";
     });
-  });
+  };
 
-  const switchActiveImg = (() => {
+  const switchActiveImg = () => {
     if (playerOne.currentlyActive === true) {
       playerOnePic.style.removeProperty("box-shadow");
       playerTwoPic.style.boxShadow = "none";
@@ -185,15 +193,15 @@ const displayController = (() => {
       playerOnePic.style.transform = "translateY(2vh)";
       playerTwoPic.style.transform = "translateY(-2vh)";
     }
-  });
+  };
 
-  const winScreen = (() => {
+  const winScreen = () => {
     const toBlur = document.querySelector("#whole");
     toBlur.style.filter = "blur(5px)";
     const winSplash = document.getElementById("winSplash");
     winSplash.style.visibility = "visible";
     winSplash.style.opacity = "1";
-  });
+  };
   return { renderBoard, clearBoard, switchActiveImg, winScreen };
 })();
 
@@ -210,38 +218,72 @@ const game = (() => {
       displayController.renderBoard().playerGame();
     } else if (playerTwo.status === "AI") {
       displayController.renderBoard().aiGame();
-    };
+    }
   };
-  const checkWinner = () => {
+  const aiMove = (isMaximizing) => {
+    const depth = (maxDepth = -1) => {
+      this.maxDepth = maxDepth;
+      this.nodesMap = new Map();
+    };
+    const minimax = (board, depth, isMaximizing) => {
+      if (depth === 0) this.nodesMap.clear();
+      if (depth === this.maxDepth) {
+        return checkWinner(true);
+      }
+  };
+};
+  const checkWinner = (intelliCheck) => {
     document.getElementById("playAgain").addEventListener("click", () => {
       resetGame();
     });
     const winner = document.getElementById("winText");
     movesMade++;
-    board.forEach ((item, index) => {
+    board.forEach((item, index) => {
       // check for horizontal wins
-      if ((index % 3) === 0 && board[index] !== "") {
-        if (board[index] === board[index + 1] && board[index] === board[index + 2]) {
-          playerOne.currentlyActive ? winner.innerText = "Player Two Wins!" : winner.innerText = "Player One Wins!";
-          displayController.winScreen();
-          return;
-        };
+      if (index % 3 === 0 && board[index] !== "") {
+        if (
+          board[index] === board[index + 1] &&
+          board[index] === board[index + 2]
+        ) {
+          if (intelliCheck === true) {
+            return board[index];
+          } else {
+            playerOne.currentlyActive
+              ? (winner.innerText = "Player Two Wins!")
+              : (winner.innerText = "Player One Wins!");
+            displayController.winScreen();
+            return;
+          }
+        }
       }
-      if ((index <= 2) && board[index] !== "") {
-        if (board[index] === board[index + 3] && board[index] === board[index + 6]) {
-          playerOne.currentlyActive ? winner.innerText = "Player Two Wins!" : winner.innerText = "Player One Wins!";
-          displayController.winScreen();
-          return;
-        };
-      };
+      if (index <= 2 && board[index] !== "") {
+        if (
+          board[index] === board[index + 3] &&
+          board[index] === board[index + 6]
+        ) {
+          if (intelliCheck === true) {
+            return board[index];
+          } else {
+            playerOne.currentlyActive
+              ? (winner.innerText = "Player Two Wins!")
+              : (winner.innerText = "Player One Wins!");
+            displayController.winScreen();
+            return;
+          }
+        }
+      }
     });
-    if ((board[0] === board[4] && board[0] === board[8]) && board[0] !== "") {
-      playerOne.currentlyActive ? winner.innerText = "Player Two Wins!" : winner.innerText = "Player One Wins!";
+    if (board[0] === board[4] && board[0] === board[8] && board[0] !== "") {
+      playerOne.currentlyActive
+        ? (winner.innerText = "Player Two Wins!")
+        : (winner.innerText = "Player One Wins!");
       displayController.winScreen();
       return;
     }
-    if ((board[2] === board[4] && board[2] === board[6]) && board[2] !== "") {
-      playerOne.currentlyActive ? winner.innerText = "Player Two Wins!" : winner.innerText = "Player One Wins!";
+    if (board[2] === board[4] && board[2] === board[6] && board[2] !== "") {
+      playerOne.currentlyActive
+        ? (winner.innerText = "Player Two Wins!")
+        : (winner.innerText = "Player One Wins!");
       displayController.winScreen();
       return;
     }
